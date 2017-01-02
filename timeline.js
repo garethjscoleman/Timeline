cf0b07be56b4bd650e439871e0494121174a9852
garethjscoleman <garethjscoleman@yahoo.co.uk>
2017-01-02 21:09:43 +0000

    Update timeline.js

--- timeline.js
+++ timeline.js
@@ -3,7 +3,7 @@ function timelineCtrl($scope,$http) {
  
 $scope.scale=1;
  $scope.divheight=function(date2, date1){
- return  (parseInt(50*$scope.scale/2,10 )+parseInt($scope.scale/16* ((date2/1000)-(date1/1000)) /((60*60*24)),10)).toString()+'px';
+ return  (parseInt(10*$scope.scale/2,10 )+parseInt($scope.scale/16* ((date2/1000)-(date1/1000)) /((60*60*24)),10)).toString()+'px';
  }
  
  $scope.timelineEvents=[
