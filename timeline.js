516d606865ad23d0e4d6c8c331d23b9ae2afe4c4
garethjscoleman <garethjscoleman@yahoo.co.uk>
2017-01-08 16:24:37 +0000

    Update timeline.js

--- timeline.js
+++ timeline.js
@@ -2,7 +2,7 @@ var timeline = angular.module('timeline', ['ngMaterial']);
 
 timeline.controller('timelineCtrl', ['$scope', '$http', function($scope,$http)
 {
- 
+$scope.title='Timeline';
 $scope.scale=1;
  $scope.divheight=function(date2, date1){
  return  (parseInt(10*$scope.scale/2,10 )+parseInt($scope.scale/16* ((date2/1000)-(date1/1000)) /((60*60*24)),10)).toString()+'px';
