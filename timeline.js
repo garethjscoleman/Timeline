d7c42d556ddd9a19060556f9256557248292c424
garethjscoleman <garethjscoleman@yahoo.co.uk>
2017-01-10 14:57:36 +0000

    Update timeline.js

--- timeline.js
+++ timeline.js
@@ -58,7 +58,7 @@ this.scale=1;
  ];
 //retrieve the events
 $http.jsonp( 'https://spreadsheets.google.com/feeds/list/1kOA4RNBdGbcleiH8Q8yhc_YD8HHeIluH7opTzTPZYcw/od6/public/values?alt=json-in-script&callback=JSON_CALLBACK'
-).then(function successCallback(response) {
+).then(angular.bind(this,function successCallback(response) {
     // this callback will be called asynchronously
  angular.forEach(response.data.feed.entry,function(value,key){
  //put the events in the events object
@@ -78,7 +78,7 @@ $http.jsonp( 'https://spreadsheets.google.com/feeds/list/1kOA4RNBdGbcleiH8Q8yhc_
   }, function errorCallback(response) {
     // called asynchronously if an error occurs
     // or server returns response with an error status.
-  });
+  }));
  return this;
 }
 ]);
