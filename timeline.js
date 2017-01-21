a52ddb5398c7225d3e9da2d2b1dd026f86646443
garethjscoleman <garethjscoleman@yahoo.co.uk>
2017-01-21 10:33:28 +0000

    Update timeline.js

--- timeline.js
+++ timeline.js
@@ -116,7 +116,7 @@ $http.jsonp( 'https://spreadsheets.google.com/feeds/list/1kOA4RNBdGbcleiH8Q8yhc_
   thetimeline.timelineEvents.push(value);
  });
   this.filteredEvents=$filter('orderBy')($filter('filter')(this.timelineEvents,this.thefilter),'whendate');
-  thetimeline.theselectedevent = thetimeline.filteredEvents[0];
+  thetimeline.theselectedevent = this.filteredEvents[0];
  //put the 
     // when the response is available
   }, function errorCallback(response) {
