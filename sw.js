06af1f4e964d207d745241fe1f57a0393a021307
garethjscoleman <garethjscoleman@rockpapersquid.com>
2017-05-05 21:32:10 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,64 +1,67 @@
 let log = console.log.bind(console);
 let err = console.error.bind(console);
- 
+
 let version = '1';
 let cacheName = 'pwa-client-v' + version;
 let dataCacheName = 'pwa-client-data-v' + version;
 let appShellFilesToCache = [
-  './',
-  './index.html',
-  'https://spreadsheets.google.com/feeds/list/1kOA4RNBdGbcleiH8Q8yhc_YD8HHeIluH7opTzTPZYcw/od6/public/values?alt=json-in-script&callback=JSON_CALLBACK'
+    './',
+    './index.html',
+    'https://spreadsheets.google.com/feeds/list/1kOA4RNBdGbcleiH8Q8yhc_YD8HHeIluH7opTzTPZYcw/od6/public/values?alt=json-in-script&callback=JSON_CALLBACK'
 ];
- 
+
 self.addEventListener('install', (e) => {
-  e.waitUntil(self.skipWaiting());
-  log('Service Worker: Installed');
- 
-  e.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      log('Service Worker: Caching App Shell');
-      return cache.addAll(appShellFilesToCache);
-    })
-  );
+    e.waitUntil(self.skipWaiting());
+    log('Service Worker: Installed');
+
+    e.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            log('Service Worker: Caching App Shell');
+            return cache.addAll(appShellFilesToCache);
+        })
+    );
 });
- 
+
 self.addEventListener('activate', (e) => {
-  e.waitUntil(self.clients.claim());
-  log('Service Worker: Active');
- 
-  e.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
- 
-        if (key !== cacheName) {
-          log('Service Worker: Removing old cache', key);
-          return caches.delete(key);
-        }
- 
-      }));
-    })
-  );
+    e.waitUntil(self.clients.claim());
+    log('Service Worker: Active');
+
+    e.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+
+                if (key !== cacheName) {
+                    log('Service Worker: Removing old cache', key);
+                    return caches.delete(key);
+                }
+
+            }));
+        })
+    );
 });
- 
+
 self.addEventListener('fetch', (e) => {
-  log('Service Worker: Fetch URL ', e.request.url);
- 
-  // Match requests for data and handle them separately
-  e.respondWith(
-    caches.match(e.request.clone()).then((response) => {
-      return response || fetch(e.request.clone()).then((r2) => {
-          return caches.open(dataCacheName).then((cache) => {
-            log('Service Worker: Fetched & Cached URL ', e.request.url);
-            cache.put(e.request.url, r2.clone());
-            return r2.clone();
-          }).catch(function() {
-               // Do nothing.
-               log('Service Worker: something went wrong with a fetch');
-          });
-        }).catch(function() {
-               // Do nothing.
-               log('Service Worker: perhaps something went wrong with a fetch');
-          });;
-    })
-  );
+    log('Service Worker: Fetch URL ', e.request.url);
+
+    // Match requests for data and handle them separately
+    e.respondWith(
+        caches.match(e.request.clone()).then((response) => {
+            return response || fetch(e.request.clone()).then((r2) => {
+                return caches.open(dataCacheName).then((cache) => {
+                    log('Service Worker: Fetched & Cached URL ', e.request.url);
+                    cache.put(e.request.url, r2.clone());
+                    return r2.clone();
+                }).catch(function() {
+                    // Do nothing.
+                    log('Service Worker: something went wrong with a fetch');
+                });
+            }).catch(function() {
+                // Do nothing.
+                log('Service Worker: perhaps something went wrong with a fetch');
+            });
+        });
+    ).catch(function() {
+        // Do nothing.
+        log('Service Worker: perhaps something went wrong with a fetch');
+    });
 });
