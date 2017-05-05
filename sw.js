e97cdfddeb129cc5eee298fb3b6b57ffccdbf41d
garethjscoleman <garethjscoleman@rockpapersquid.com>
2017-05-06 00:31:44 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -49,22 +49,35 @@ self.addEventListener('fetch', (e) => {
       log('Service Worker: Fetch URL ', e.request.url);
       var dataUrl = 'https://spreadsheets.google.com/feeds/list/1kOA4RNBdGbcleiH8Q8yhc_YD8HHeIluH7opTzTPZYcw/od6/public/values?alt=json-in-script&callback=angular.callbacks._0';
       if (e.request.url.indexOf(dataUrl) > -1) {
-        /*
-         * When the request URL contains dataUrl, the app is asking for fresh
-         * data. In this case, the service worker always goes to the
-         * network and then caches the response. This is called the "Cache then
-         * network" strategy:
-         * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
-         */
-        e.respondWith(
-          caches.open(dataCacheName).then(function(cache) {
-            return fetch(e.request, {mode: 'no-cors'}).then(function(response){
-              cache.put(e.request.url, response);
-              log('Service Worker: Fetched & Cached URL ', e.request.url);
-              return response;
-            });
-          })
-        );
+          
+         //this time just skip trying to cache this
+          if(1===1){
+              e.respondWith(
+                   return fetch(e.request).then(function(response){
+                      log('Service Worker: Fetched but not cached URL ', e.request.url);
+                      return response;
+                    });
+                  );
+         }
+         else
+         {
+            /*
+             * When the request URL contains dataUrl, the app is asking for fresh
+             * data. In this case, the service worker always goes to the
+             * network and then caches the response. This is called the "Cache then
+             * network" strategy:
+             * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
+             */
+            e.respondWith(
+              caches.open(dataCacheName).then(function(cache) {
+                return fetch(e.request, {mode: 'no-cors'}).then(function(response){
+                  cache.put(e.request.url, response);
+                  log('Service Worker: Fetched & Cached URL ', e.request.url);
+                  return response;
+                });
+              })
+            );
+        }
       } else {
         // Match requests for data and handle them separately
         e.respondWith(
