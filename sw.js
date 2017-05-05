89920aa365a11a43ef8fdf83b44f4a35c086a7f9
garethjscoleman <garethjscoleman@rockpapersquid.com>
2017-05-05 22:47:10 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,6 +10,13 @@ let appShellFilesToCache = [
     'https://spreadsheets.google.com/feeds/list/1kOA4RNBdGbcleiH8Q8yhc_YD8HHeIluH7opTzTPZYcw/od6/public/values?alt=json-in-script&callback=JSON_CALLBACK'
 ];
 
+function handleErrors(response) {
+    if (!response.ok) {
+        throw Error(response.statusText);
+    }
+    return response;
+}
+
 self.addEventListener('install', (e) => {
     e.waitUntil(self.skipWaiting());
     log('Service Worker: Installed');
@@ -51,15 +58,21 @@ self.addEventListener('fetch', (e) => {
    
 
             // respond from the cache, or the network
-            var fetchPromise = fetch(e.request.clone()).then((networkResponse) => {
+            var fetchPromise = fetch(e.request.clone()).then(handleErrors).then((networkResponse) => {
+                
                 log('Response for',e.request.url, 'was', networkResponse.ok);
+                
                 var theresponse = caches.open(dataCacheName).then((cache) => {
                     log('Service Worker: Fetched & Cached URL ', e.request.url);
                     cache.put(e.request.url, networkResponse.clone());
                     return networkResponse.clone();
                 });
                 return theresponse;
-            });
+            }).catch(){
+            log('Bad Response from' + e.request.url + '?');
+            }
+            
+            ;
             return response || fetchPromise;
         })
     );
