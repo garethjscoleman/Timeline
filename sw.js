6fd0093d9ef74a4b536b7d8b0a5bea814fc1d119
garethjscoleman <garethjscoleman@rockpapersquid.com>
2017-05-05 23:52:14 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -46,29 +46,47 @@ self.addEventListener('activate', (e) => {
 });
 
 self.addEventListener('fetch', (e) => {
-    log('Service Worker: Fetch URL ', e.request.url);
+      log('Service Worker: Fetch URL ', e.request.url);
+      var dataUrl = 'https://spreadsheets.google.com/feeds/list/1kOA4RNBdGbcleiH8Q8yhc_YD8HHeIluH7opTzTPZYcw/od6/public/values?alt=json-in-script&callback=angular.callbacks._0';
+      if (e.request.url.indexOf(dataUrl) > -1) {
+        /*
+         * When the request URL contains dataUrl, the app is asking for fresh
+         * data. In this case, the service worker always goes to the
+         * network and then caches the response. This is called the "Cache then
+         * network" strategy:
+         * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
+         */
+        e.respondWith(
+          caches.open(dataCacheName).then(function(cache) {
+            return fetch(e.request).then(function(response){
+              cache.put(e.request.url, response.clone());
+              return response;
+            });
+          })
+        );
+      } else {
+        // Match requests for data and handle them separately
+        e.respondWith(
+            caches.match(e.request.clone()).then((response) => {
 
-    // Match requests for data and handle them separately
-    e.respondWith(
-        caches.match(e.request.clone()).then((response) => {
-           
-            
-   
 
-            // respond from the cache, or the network
-            var fetchPromise = fetch(e.request.clone()).then((networkResponse) => {
-                
-                log('Response for',e.request.url, 'was', networkResponse.ok);
-                if(networkResponse.ok){
-                    var theresponse = caches.open(dataCacheName).then((cache) => {
-                        log('Service Worker: Fetched & Cached URL ', e.request.url);
-                        cache.put(e.request.url, networkResponse.clone());
-                        return networkResponse.clone();
-                    });
-                    return theresponse;
-                }
-            }).catch(error => log(error));
-            return response || fetchPromise;
-        })
-    );
+
+
+                // respond from the cache, or the network
+                var fetchPromise = fetch(e.request.clone()).then((networkResponse) => {
+
+                    log('Response for',e.request.url, 'was', networkResponse.ok);
+                    if(networkResponse.ok){
+                        var theresponse = caches.open(dataCacheName).then((cache) => {
+                            log('Service Worker: Fetched & Cached URL ', e.request.url);
+                            cache.put(e.request.url, networkResponse.clone());
+                            return networkResponse.clone();
+                        });
+                        return theresponse;
+                    }
+                }).catch(error => log(error));
+                return response || fetchPromise;
+            })
+        );
+      }
 });
