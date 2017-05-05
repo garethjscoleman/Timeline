360e1d847c3c32b67295a26539a92508dd4ea51d
garethjscoleman <garethjscoleman@rockpapersquid.com>
2017-05-05 23:27:17 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ let dataCacheName = 'pwa-client-data-v' + version;
 let appShellFilesToCache = [
     './',
     './index.html',
-    'https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular.min.js'
+    'https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular.min.js',
     'https://spreadsheets.google.com/feeds/list/1kOA4RNBdGbcleiH8Q8yhc_YD8HHeIluH7opTzTPZYcw/od6/public/values?alt=json-in-script&callback=JSON_CALLBACK'
 ];
 
