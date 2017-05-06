5060e1e71cf3eb037dd15727162da24b41b6ece1
garethjscoleman <garethjscoleman@rockpapersquid.com>
2017-05-06 01:51:36 +0100

    Update timeline.js

--- timeline.js
+++ timeline.js
@@ -106,21 +106,23 @@ timeline.controller('timelineCtrl', ['$scope', '$filter', '$http', '$mdDialog', 
 
     $http.jsonp('https://spreadsheets.google.com/feeds/list/1kOA4RNBdGbcleiH8Q8yhc_YD8HHeIluH7opTzTPZYcw/od6/public/values?alt=json-in-script&callback=JSON_CALLBACK').then(function successCallback(response) {
         // this callback will be called asynchronously
-        angular.forEach(response.data.feed.entry, function(value, key) {
-            //put the events in the events object
-            value.when = value.gsx$date.$t;
-            value.whendate = Date.parse(value.when);
-            value.event = value.gsx$event.$t;
-            value.who = value.gsx$who.$t;
-            value.where = value.gsx$where.$t;
-            value.category = value.gsx$category.$t;
-            value.notes = value.gsx$notes.$t;
-            thetimeline.timelineEvents.push(value);
-        });
-        localStorage.setItem('events', JSON.stringify(thetimeline.timelineEvents));
-        thetimeline.filteredEvents = $filter('orderBy')($filter('filter')(thetimeline.timelineEvents, this.thefilter), 'whendate');
-        thetimeline.theselectedevent = thetimeline.filteredEvents[0];
-        //put the 
+        if (!response.data.offline){
+            angular.forEach(response.data.feed.entry, function(value, key) {
+                //put the events in the events object
+                value.when = value.gsx$date.$t;
+                value.whendate = Date.parse(value.when);
+                value.event = value.gsx$event.$t;
+                value.who = value.gsx$who.$t;
+                value.where = value.gsx$where.$t;
+                value.category = value.gsx$category.$t;
+                value.notes = value.gsx$notes.$t;
+                thetimeline.timelineEvents.push(value);
+            });
+            localStorage.setItem('events', JSON.stringify(thetimeline.timelineEvents));
+            thetimeline.filteredEvents = $filter('orderBy')($filter('filter')(thetimeline.timelineEvents, this.thefilter), 'whendate');
+            thetimeline.theselectedevent = thetimeline.filteredEvents[0];
+            }
+            //put the 
         // when the response is available
     }, function errorCallback(response) {
         // called asynchronously if an error occurs
